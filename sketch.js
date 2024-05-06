bc5df2891ac03d9c6a724204f2e6630917647fb1
jonathanj20 <jonathanpossada@gmail.com>
2024-05-06 11:42:25 -0700

    Se hizo la función punto-pendiente

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ Luis Gabriel Casillas Moreno
 Graficacion y Multimedia Examen Unidad 2
 IDS 8 T.M.
 */
-let timeZoneDifferences = [0, +1, +9]; 
+let timeZoneDifferences = [0, +1, +9];
 let hourOffset = 0;
 let minuteOffset = 0;
 let inputTime;
@@ -14,14 +14,14 @@ let inputButton;
 function setup() {
   createCanvas(600, 250);
   angleMode(DEGREES);
-  
+
   inputTime = createInput("", "text");
   inputTime.position(20, height + 20);
-  
+
   inputButton = createButton('Actualizar');
   inputButton.position(inputTime.x + inputTime.width + 10, height + 20);
   inputButton.mousePressed(updateClocks);
-  inputTime.changed(updateClocks); 
+  inputTime.changed(updateClocks);
 }
 
 function draw() {
@@ -39,12 +39,12 @@ function drawClock(x, y, h, m, s, type, city) {
   stroke(0);
   noFill();
   ellipse(x, y, 80, 80);
-  
+
   textSize(14);
   textAlign(CENTER, CENTER);
   fill(0);
   text(city, x, y - 50);
-  
+
   if (type === 0) {
     drawHandPuntoPendiente(x, y, map(s, 0, 60, 0, 360) - 90, 30, color(255, 0, 0));
     drawHandPuntoPendiente(x, y, map(m, 0, 60, 0, 360) - 90 + map(s, 0, 60, 0, 360) / 60, 25, color(0, 255, 0));
@@ -58,4 +58,13 @@ function drawClock(x, y, h, m, s, type, city) {
     drawHandPuntoPendiente(x, y, map(m, 0, 60, 0, 360) - 90 + map(s, 0, 60, 0, 360) / 60, 25, color(0, 255, 0)); // Minutos
     drawHandPuntoPendiente(x, y, map(h % 12, 0, 12, 0, 360) - 90 + map(m, 0, 60, 0, 360) / 12, 20, color(0, 0, 255)); // Horas
   }
+}
+
+function drawHandPuntoPendiente(x, y, angle, length, color) {
+  let endX = x + cos(angle) * length;
+  let endY = y + sin(angle) * length;
+
+  stroke(color);
+  strokeWeight(2);
+  line(x, y, endX, endY);
 }
